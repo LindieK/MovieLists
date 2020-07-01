cdedf61d95780189b991f06e524cb395bc0bd143
Linda Okorie <lindaokorie27@gmail.com>
2020-07-01 23:02:11 +0100

    Updates

---
+++ precache-manifest.6f1f28ac905f251b2c38b212cca36810.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "388b49cf1ba8b79e059f542bcaa08315",
+    "url": "/MovieLists/index.html"
+  },
+  {
+    "revision": "5d4f72802d3cde44f69f",
+    "url": "/MovieLists/static/css/main.a8383dd0.chunk.css"
+  },
+  {
+    "revision": "ec27219f2965abcffb38",
+    "url": "/MovieLists/static/js/2.d605a265.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/MovieLists/static/js/2.d605a265.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d4f72802d3cde44f69f",
+    "url": "/MovieLists/static/js/main.92f819e2.chunk.js"
+  },
+  {
+    "revision": "80e56e249490b577e9fe",
+    "url": "/MovieLists/static/js/runtime-main.6300bedd.js"
+  }
+]);
